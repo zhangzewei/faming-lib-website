61d6281ef16d114b6ac7497ed5b2fa3708fe115d
zhangzewei <453646474@qq.com>
2018-05-16 17:30:02 +0800

    feat: client页面框架搭建

--- client/src/containers/Client/index.js
+++ client/src/containers/Client/index.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
+import { Layout, Row, Col } from 'antd';
+import './style.css';
+
+const { Header, Footer, Sider, Content } = Layout;
 
 export default class Client extends Component {
   render() {
     return (
-      <div>
-        <h2>前端展示页面</h2>
-        <Link to="/admin/login">后端管理系统</Link>
-      </div>
+      <Layout>
+        <Header className="client-header" />
+        <Content className="client-container">
+          <Row>
+            <Col xs={2} sm={4} md={6} lg={8} xl={10}>Col</Col>
+          </Row>
+          <Link to="/admin/login">后端管理系统</Link>
+        </Content>
+        <Footer className="client-footer">Footer</Footer>
+      </Layout>
     )
   }
 }
